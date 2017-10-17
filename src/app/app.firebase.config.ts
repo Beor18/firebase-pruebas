94d33b54b3cd6965467bb023b332df68f8df20ee
Fernando Lopez <fer.ragnar19@gmail.com>
2017-10-17 09:49:09 -0300

    configuracion firebase
    Signed-off-by: Fernando Lopez <fer.ragnar19@gmail.com>

---
+++ src/app/app.firebase.config.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyBPPa1BjTvdrFlo41no1Yt71vTpld-m9As",
+    authDomain: "flybynetfer.firebaseapp.com",
+    databaseURL: "https://flybynetfer.firebaseio.com",
+    projectId: "flybynetfer",
+    storageBucket: "flybynetfer.appspot.com",
+    messagingSenderId: "1002706209766"
+  }; 
