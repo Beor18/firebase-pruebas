1a4082a607f23f2a13e4e3a87ec08a399f87d6d8
Fernando Lopez <fer.ragnar19@gmail.com>
2017-10-17 13:43:46 -0300

    implementacion home
    Signed-off-by: Fernando Lopez <fer.ragnar19@gmail.com>

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,35 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
 
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
+import { AngularFireAuth } from 'angularfire2/auth';
+
+
+@IonicPage()
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(private afAuth: AngularFireAuth, private toast: ToastController,
+    public navCtrl: NavController, public navParams: NavParams) {
   }
 
+  //Mensaje al entrar
+  ionViewWillLoad() {
+    this.afAuth.authState.subscribe(data => {
+      if (data && data.email && data.uid) {
+        this.toast.create({
+          message: `Bienvenido a App prueba firebase, ${data.email}`,
+          duration: 3000
+        }).present();
+      }
+      else {
+        this.toast.create({
+          message: `Un mensaje.`,
+          duration: 3000
+        }).present();
+      }
+    })
+  }
 }
