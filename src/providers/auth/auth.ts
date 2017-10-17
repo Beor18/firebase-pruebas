47442408a09818b724bb999d4f6075849d0880ba
Fernando Lopez <fer.ragnar19@gmail.com>
2017-10-17 20:38:22 -0300

    agregado provider auth
    Signed-off-by: Fernando Lopez <fer.ragnar19@gmail.com>

---
+++ src/providers/auth/auth.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+//import * as firebase from 'firebase/app';
+
+
+@Injectable()
+export class AuthProvider {
+
+  constructor(private afAuth :  AngularFireAuth) {
+    console.log('Hello AuthProvider Provider');
+  }
+
+ 
+
+ // Logout de usuario
+ logout(){
+   this.afAuth.auth.signOut().then(()=>{
+     // hemos salido
+   })
+ }
+
+// Devuelve la session
+ get Session(){
+  return this.afAuth.authState;
+ }
+
+}
