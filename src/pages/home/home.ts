f2764c8f667032c0e098beb5713d7250b08d1ee8
Fernando Lopez <fer.ragnar19@gmail.com>
2017-10-17 20:39:53 -0300

    cerrar sesion
    Signed-off-by: Fernando Lopez <fer.ragnar19@gmail.com>

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
 import { AngularFireAuth } from 'angularfire2/auth';
+import { AuthProvider } from '../../providers/auth/auth';
 
 
 @IonicPage()
@@ -11,7 +12,7 @@ import { AngularFireAuth } from 'angularfire2/auth';
 export class HomePage {
 
   constructor(private afAuth: AngularFireAuth, private toast: ToastController,
-    public navCtrl: NavController, public navParams: NavParams) {
+    public navCtrl: NavController, public navParams: NavParams, public auth : AuthProvider) {
   }
 
   //Mensaje al entrar
@@ -31,4 +32,8 @@ export class HomePage {
       }
     })
   }
+
+  cerrarSesion(){
+    this.auth.logout();
+}
 }
