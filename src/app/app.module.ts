a371117de7a7126a46a4ba654aab9972871da0e1
Fernando Lopez <fer.ragnar19@gmail.com>
2017-10-19 21:58:18 -0300

    cambios
    Signed-off-by: Fernando Lopez <fer.ragnar19@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,8 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-import { HttpModule } from '@angular/http';
+//import { HttpModule } from '@angular/http';
+import { NotasService } from "../services/notas.service";
 
 //Cargamos modulos Firebase y archivo config
 import { AngularFireModule } from 'angularfire2';
@@ -14,15 +15,19 @@ import { MyApp } from './app.component';
 import { FIREBASE_CONFIG } from "./app.firebase.config";
 import { AuthProvider } from '../providers/auth/auth';
 import { AccordionComponent } from '../components/accordion/accordion';
+import { DetallesPage } from "../pages/detalles/detalles";
+//import { HomePage } from "../pages/home/home";
 
 @NgModule({
   declarations: [
     MyApp,
-    AccordionComponent
+    //HomePage,
+    AccordionComponent,
+    DetallesPage
   ],
   imports: [
     BrowserModule,
-    HttpModule,
+    //HttpModule,
     IonicModule.forRoot(MyApp),
     AngularFireModule.initializeApp(FIREBASE_CONFIG),
     AngularFireAuthModule,
@@ -30,13 +35,15 @@ import { AccordionComponent } from '../components/accordion/accordion';
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp
+    MyApp,
+    DetallesPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
-    AuthProvider
+    AuthProvider,
+    NotasService
   ]
 })
 export class AppModule {}
